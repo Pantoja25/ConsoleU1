902ee747df7bacd06588a0344ec8e79b5172043d
Pantoja25 <165227131+Pantoja25@users.noreply.github.com>
2024-06-13 21:24:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace consoleU1 {
+	internal class Program {
+		static void Main(string[] args) {
+			Usuario usuario = new Usuario();
+			usuario.nome = "José";
+			usuario.login = "root";
+			usuario.senha = "qwe123";
+
+			string login;
+			string senha;
+
+			do {
+				Console.WriteLine("Digite seu LOGIN: ");
+				login = Console.ReadLine();
+
+				Console.WriteLine("Digite sua SENHA: ");
+				senha = Console.ReadLine();
+
+			} while (!usuario.Logar(login, senha));
+
+		}
+	}
+}
---
+++ Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleU1 {
+	internal class Usuario {
+		//Os atributos: nome, login, senha
+		//Cadeia = String
+		//INT = Inteiro
+		//Float = Real
+		public string nome;
+		public string login;
+		public string senha;
+		public int qtdLogin;
+		public float peso;
+		public bool ehsolteiro;
+		public char abrvNomeMeio;
+
+		public bool Logar(string _login, string _senha) {
+			if (_login == login) {
+				if (_senha == senha) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
